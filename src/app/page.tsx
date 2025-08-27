0d8f3010f8d77abfb90059d31ef1429c55292384
Ross Tungol <rossjstungol@gmail.com>
2025-08-27 20:03:46 +0800

    Theme setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { SignInButton, UserButton } from '@clerk/nextjs'
+import { ThemeToggle } from '@/components/ThemeToggle'
 
 export default function HomePage() {
   return (
-    <>
-      <SignInButton />
-      <UserButton />
-    </>
+    <div className='p-4 space-y-4'>
+      <div className='flex items-center gap-4'>
+        <SignInButton />
+        <UserButton />
+        <ThemeToggle />
+      </div>
+    </div>
   )
 }
---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { Monitor, Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { cn } from '@/lib/utils'
+
+const themes = [
+  {
+    name: 'Light',
+    Icon: Sun,
+    value: 'light',
+  },
+  {
+    name: 'Dark',
+    Icon: Moon,
+    value: 'dark',
+  },
+  {
+    name: 'System',
+    Icon: Monitor,
+    value: 'system',
+  },
+] as const
+
+export function ThemeToggle() {
+  const [mounted, setMounted] = useState(false)
+  const { setTheme, theme, resolvedTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null // Prevents hydration mismatch
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='ghost' size='icon'>
+          {resolvedTheme === 'dark' ? <Moon /> : <Sun />}
+          <span className='sr-only'>Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        {themes.map(({ value, Icon, name }) => (
+          <DropdownMenuItem
+            key={value}
+            onClick={() => setTheme(value)}
+            className={cn(
+              'cursor-pointer',
+              theme === value && 'bg-accent text-accent-foreground'
+            )}
+          >
+            <Icon className='size-4' />
+            {name}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
