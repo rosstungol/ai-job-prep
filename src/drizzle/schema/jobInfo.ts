63ce7162ac6bcba0b7f812c88cd44b63c2fc1b4c
Ross Tungol <rossjstungol@gmail.com>
2025-08-28 20:11:41 +0800

    Database setup

---
+++ src/drizzle/schema/jobInfo.ts
@@ -0,0 +1,35 @@
+import { relations } from 'drizzle-orm'
+import { pgEnum, pgTable, varchar } from 'drizzle-orm/pg-core'
+import { createdAt, id, updatedAt } from '../schemaHelpers'
+import { UserTable } from './user'
+import { QuestionTable } from './question'
+import { InterviewTable } from './interview'
+
+export const experienceLevels = ['junior', 'mid-level', 'senior'] as const
+export type ExperienceLevel = (typeof experienceLevels)[number]
+export const experienceLevelEnum = pgEnum(
+  'job_infos_experience_level',
+  experienceLevels
+)
+
+export const JobInfoTable = pgTable('job-info', {
+  id,
+  userId: varchar()
+    .references(() => UserTable.id, { onDelete: 'cascade' })
+    .notNull(),
+  title: varchar(),
+  name: varchar().notNull(),
+  experienceLevel: experienceLevelEnum().notNull(),
+  description: varchar().notNull(),
+  createdAt,
+  updatedAt,
+})
+
+export const jobInfoRelations = relations(JobInfoTable, ({ one, many }) => ({
+  user: one(UserTable, {
+    fields: [JobInfoTable.userId],
+    references: [UserTable.id],
+  }),
+  questions: many(QuestionTable),
+  interviews: many(InterviewTable),
+}))
---
+++ src/drizzle/schema/user.ts
@@ -0,0 +1,17 @@
+import { relations } from 'drizzle-orm'
+import { pgTable, varchar } from 'drizzle-orm/pg-core'
+import { createdAt, updatedAt } from '../schemaHelpers'
+import { JobInfoTable } from './jobInfo'
+
+export const UserTable = pgTable('users', {
+  id: varchar().primaryKey(),
+  email: varchar().notNull().unique(),
+  name: varchar().notNull(),
+  imageUrl: varchar().notNull(),
+  createdAt,
+  updatedAt,
+})
+
+export const userRelations = relations(UserTable, ({ many }) => ({
+  jobInfos: many(JobInfoTable),
+}))
