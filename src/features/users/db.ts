28f4e33f2f78f58b0b4d86f91828de697ea8bc0d
Ross Tungol <rossjstungol@gmail.com>
2025-08-30 01:59:02 +0800

    Clerk webhook setup

---
+++ src/features/users/db.ts
@@ -0,0 +1,14 @@
+import { eq } from 'drizzle-orm'
+import { db } from '@/drizzle/db'
+import { UserTable } from '@/drizzle/schema'
+
+export async function upsertUser(user: typeof UserTable.$inferInsert) {
+  await db
+    .insert(UserTable)
+    .values(user)
+    .onConflictDoUpdate({ target: [UserTable.id], set: user })
+}
+
+export async function deleteUser(id: string) {
+  await db.delete(UserTable).where(eq(UserTable.id, id))
+}
