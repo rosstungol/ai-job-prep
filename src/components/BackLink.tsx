3ebf615ce927dbe58e4c9ee02bf3377e0c3bd538
Ross Tungol <rossjstungol@gmail.com>
2025-09-03 04:53:25 +0800

    Implements the Job info new page

---
+++ src/components/BackLink.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { ArrowLeftIcon } from 'lucide-react'
+import { Button } from './ui/button'
+import { cn } from '@/lib/utils'
+
+export function BackLink({
+  href,
+  children,
+  className,
+}: {
+  href: string
+  children: React.ReactNode
+  className?: string
+}) {
+  return (
+    <Button
+      asChild
+      variant='ghost'
+      size='sm'
+      className={cn('-ml-3', className)}
+    >
+      <Link
+        href={href}
+        className='flex gap-2 items-center text-sm text-muted-foreground'
+      >
+        <ArrowLeftIcon />
+        {children}
+      </Link>
+    </Button>
+  )
+}
