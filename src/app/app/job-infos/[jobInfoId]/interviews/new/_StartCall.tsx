24837bd1cf8d59f955c3c6dd68d4bc4601b51b7c
Ross Tungol <rossjstungol@gmail.com>
2025-09-10 20:18:21 +0800

    Implements the Interview chat controls

--- src/app/app/job-infos/[jobInfoId]/interviews/new/_StartCall.tsx
+++ src/app/app/job-infos/[jobInfoId]/interviews/new/_StartCall.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Loader2Icon, MicIcon, MicOffIcon, PhoneOffIcon } from 'lucide-react'
 import { useVoice, VoiceReadyState } from '@humeai/voice-react'
 import { env } from '@/data/env/client'
 import { JobInfoTable } from '@/drizzle/schema'
@@ -54,8 +55,81 @@ export function StartCall({
     readyState === VoiceReadyState.CONNECTING ||
     readyState === VoiceReadyState.CLOSED
   ) {
-    return null
+    return (
+      <div className='h-screen-header flex items-center justify-center'>
+        <Loader2Icon className='size-24 animate-spin m-auto' />
+      </div>
+    )
   }
 
-  return 'Connected'
+  return (
+    <div className='overflow-y-auto h-screen-header flex flex-col-reverse'>
+      <div className='container py-6 flex flex-col items-center justify-end'>
+        <Messages user={user} />
+        <Controls />
+      </div>
+    </div>
+  )
+}
+
+function Messages({
+  user,
+}: {
+  user: {
+    name: string
+    imageUrl: string
+  }
+}) {
+  return null
+}
+
+function Controls() {
+  const { disconnect, isMuted, mute, unmute, micFft, callDurationTimestamp } =
+    useVoice()
+
+  return (
+    <div className='flex gap-5 rounded border px-5 py-2 w-fit sticky bottom-6 bg-background items-center'>
+      <Button
+        variant='ghost'
+        size='icon'
+        className='-mx-3'
+        onClick={() => (isMuted ? unmute() : mute())}
+      >
+        {isMuted ? <MicOffIcon className='text-destructive' /> : <MicIcon />}
+        <span className='sr-only'>{isMuted ? 'Unmute' : 'Mute'}</span>
+      </Button>
+      <div className='self-stretch'>
+        <FftVisualizer fft={micFft} />
+      </div>
+      <div className='text-sm text-muted-foreground tabular-nums'>
+        {callDurationTimestamp}
+      </div>
+      <Button
+        variant='ghost'
+        size='icon'
+        className='-mx-3'
+        onClick={disconnect}
+      >
+        <PhoneOffIcon className='text-destructive' />
+        <span className='sr-only'>End call</span>
+      </Button>
+    </div>
+  )
+}
+
+function FftVisualizer({ fft }: { fft: number[] }) {
+  return (
+    <div className='flex gap-1 items-center h-full'>
+      {fft.map((value, index) => {
+        const percent = (value / 4) * 100
+        return (
+          <div
+            key={index}
+            className='min-h-0.5 bg-primary/75 w-0.5 rounded'
+            style={{ height: `${percent < 10 ? 0 : percent}%` }}
+          ></div>
+        )
+      })}
+    </div>
+  )
 }
