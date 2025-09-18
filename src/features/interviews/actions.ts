2b152e951e847387f3c493ded6db494c049cdc48
Ross Tungol <rossjstungol@gmail.com>
2025-09-18 18:39:42 +0800

    Updates the Interview actions

--- src/features/interviews/actions.ts
+++ src/features/interviews/actions.ts
@@ -3,8 +3,9 @@
 import { cacheTag } from 'next/dist/server/use-cache/cache-tag'
 import { and, eq } from 'drizzle-orm'
 import { db } from '@/drizzle/db'
-import { insertInterview } from './db'
-import { JobInfoTable } from '@/drizzle/schema'
+import { insertInterview, updateInterview as updateInterviewDB } from './db'
+import { InterviewTable, JobInfoTable } from '@/drizzle/schema'
+import { getInterviewIdTag } from './dbCache'
 import { getJobInfoIdTag } from '../jobInfos/dbCache'
 import { getCurrentUser } from '@/services/clerk/lib/getCurrentUser'
 
@@ -41,6 +42,38 @@ export async function createInterview({
   }
 }
 
+export async function updateInterview(
+  id: string,
+  data: {
+    humeChatId?: string
+    duration?: string
+  }
+) {
+  const { userId } = await getCurrentUser()
+
+  if (userId == null) {
+    return {
+      error: true,
+      message: "You don't have permission to do this.",
+    }
+  }
+
+  const interview = await getInterview(id, userId)
+
+  if (interview == null) {
+    return {
+      error: true,
+      message: "You don't have permission to do this.",
+    }
+  }
+
+  await updateInterviewDB(id, data)
+
+  return {
+    error: false,
+  }
+}
+
 async function getJobInfo(id: string, userId: string) {
   'use cache'
   cacheTag(getJobInfoIdTag(id))
@@ -48,4 +81,29 @@ async function getJobInfo(id: string, userId: string) {
   return db.query.JobInfoTable.findFirst({
     where: and(eq(JobInfoTable.id, id), eq(JobInfoTable.userId, userId)),
   })
+}
+
+async function getInterview(id: string, userId: string) {
+  'use cache'
+  cacheTag(getInterviewIdTag(id))
+
+  const interview = await db.query.InterviewTable.findFirst({
+    where: eq(InterviewTable.id, id),
+    with: {
+      jobInfo: {
+        columns: {
+          id: true,
+          userId: true,
+        },
+      },
+    },
+  })
+
+  if (interview == null) return null
+
+  cacheTag(getJobInfoIdTag(interview.jobInfo.id))
+
+  if (interview.jobInfo.userId !== userId) return null
+
+  return interview
 }
