8cbbf62fef4d991d84b354e27e4f542adb939d29
Ross Tungol <rossjstungol@gmail.com>
2025-09-21 00:20:33 +0800

    Adds the Interview details components

---
+++ src/components/MarkdownRenderer.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps } from 'react'
+import Markdown from 'react-markdown'
+import { cn } from '@/lib/utils'
+
+export default function MarkdownRenderer({
+  className,
+  ...props
+}: {
+  className?: string
+} & ComponentProps<typeof Markdown>) {
+  return (
+    <div
+      className={cn(
+        'max-w-none prose prose-neutral dark:prose-invert font-sans',
+        className
+      )}
+    >
+      <Markdown {...props} />
+    </div>
+  )
+}
