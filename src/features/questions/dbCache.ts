8c1ec436e44e642f27041671d307f8a08b2c0c25
Ross Tungol <rossjstungol@gmail.com>
2025-10-01 22:24:52 +0800

    Adds the Question cache tags

---
+++ src/features/questions/dbCache.ts
@@ -0,0 +1,27 @@
+import { revalidateTag } from 'next/cache'
+
+import { getGlobalTag, getIdTag, getJobInfoTag } from '@/lib/dataCache'
+
+export function getQuestionGlobalTag() {
+  return getGlobalTag('questions')
+}
+
+export function getQuestionJobInfoTag(jobInfoId: string) {
+  return getJobInfoTag('questions', jobInfoId)
+}
+
+export function getQuestionIdTag(id: string) {
+  return getIdTag('questions', id)
+}
+
+export function revalidateQuestionCache({
+  id,
+  jobInfoId,
+}: {
+  id: string
+  jobInfoId: string
+}) {
+  revalidateTag(getQuestionGlobalTag())
+  revalidateTag(getQuestionJobInfoTag(jobInfoId))
+  revalidateTag(getQuestionIdTag(id))
+}
