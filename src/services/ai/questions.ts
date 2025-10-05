595323854fb303556218b1d5df408030bd8e97d2
Ross Tungol <rossjstungol@gmail.com>
2025-10-05 21:10:58 +0800

    Adds the Question generation function

---
+++ src/services/ai/questions.ts
@@ -0,0 +1,58 @@
+import { ModelMessage, streamText } from 'ai'
+
+import {
+  JobInfoTable,
+  QuestionDifficulty,
+  QuestionTable,
+} from '@/drizzle/schema'
+
+import { google } from './models/google'
+
+export function generateAiQuestion({
+  jobInfo,
+  previousQuestions,
+  difficulty,
+  onFinish,
+}: {
+  jobInfo: Pick<
+    typeof JobInfoTable.$inferSelect,
+    'title' | 'description' | 'experienceLevel'
+  >
+  previousQuestions: Pick<
+    typeof QuestionTable.$inferSelect,
+    'text' | 'difficulty'
+  >[]
+  difficulty: QuestionDifficulty
+  onFinish: (question: string) => void
+}) {
+  const previousMessages = previousQuestions.flatMap(
+    (q) =>
+      [
+        { role: 'user', content: q.difficulty },
+        { role: 'assistant', content: q.text },
+      ] satisfies ModelMessage[]
+  )
+
+  return streamText({
+    model: google('gemini-2.5-flash'),
+    onFinish: ({ text }) => onFinish(text),
+    messages: [...previousMessages, { role: 'user', content: difficulty }],
+    system: `You are an AI assistant that creates technical interview questions tailored to a specific job role. Your task is to generate one **realistic and relevant** technical question that matches the skill requirements of the job and aligns with the difficulty level provided by the user.
+
+Job Information:
+- Job Description: \`${jobInfo.description}\`
+- Experience Level: \`${jobInfo.experienceLevel}\`
+${jobInfo.title ? `\n- Job Title: \`${jobInfo.title}\`` : ''}
+
+Guidelines:
+- The question must reflect the skills and technologies mentioned in the job description.
+- Make sure the question is appropriately scoped for the specified experience level.
+- A difficulty level of "easy", "medium", or "hard" is provided by the user and should be used to tailor the question.
+- Prefer practical, real-world challenges over trivia.
+- Return only the question, clearly formatted (e.g., with code snippets or bullet points if needed). Do not include the answer.
+- Return only one question at a time.
+- It is ok to ask a question about just a single part of the job description, such as a specific technology or skill (e.g., if the job description is for a Next.js, Drizzle, and TypeScript developer, you can ask a TypeScript only question).
+- The question should be formatted as markdown.
+- Stop generating output as soon you have provided the full question.`,
+  })
+}
