0aedcd8f1fa8c91bd495b8819676f016f34bba32
Ross Tungol <rossjstungol@gmail.com>
2025-10-15 23:08:43 +0800

    Adds caching revalidation to the User database functions

--- src/features/users/db.ts
+++ src/features/users/db.ts
@@ -3,13 +3,19 @@ import { eq } from 'drizzle-orm'
 import { db } from '@/drizzle/db'
 import { UserTable } from '@/drizzle/schema'
 
+import { revalidateUserCache } from './dbCache'
+
 export async function upsertUser(user: typeof UserTable.$inferInsert) {
   await db
     .insert(UserTable)
     .values(user)
     .onConflictDoUpdate({ target: [UserTable.id], set: user })
+
+  revalidateUserCache(user.id)
 }
 
 export async function deleteUser(id: string) {
   await db.delete(UserTable).where(eq(UserTable.id, id))
+
+  revalidateUserCache(id)
 }
